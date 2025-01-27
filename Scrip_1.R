8e1257f5cad74e90119b289eead0dd09514f1fb2
Veronica_PC <veronicahermer2015@gmail.com>
2025-01-27 13:30:57 -0600

    actualizar md

--- Scrip_1.R
+++ Scrip_1.R
@@ -1,1 +1,28 @@
- cc cc
+# Verónica Hernández Merino
+# 27/01/2025
+# Matr: 2128832
+#
+
+
+# Datos básicos -----------------------------------------------------------
+3 + 5
+8 * 4
+3 / 2
+
+suma <- 3 + 5
+suma
+suma * 4
+suma * suma
+suma * suma # si funciona por usar de forma correcta el nombre
+
+Suma * suma # Forma incorrecta, no existe suma (S)
+
+# Importar datos ----------------------------------------------------------
+edad <- c(50, 25, 30, 18, 45, 25, 36, 17, 45, 24, 
+          22, 18, 41, 43)
+edad
+mean(edad)
+var(edad)
+sd(edad)
+
+edad * c(2,3)
